f911e0d3937b95cc61c376e074f810258da1b89f
Vaishnavh <vaishnavh.nagarajan@gmail.com>
2017-03-24 18:48:55 -0400

    Cleaned up Code

--- utils.py
+++ utils.py
@@ -25,20 +25,19 @@ def plot(records):
     # Todo add median code
     
     
-def plot_log_log_summary(records,B=20,summary=np.median, discrete=False):
+def plot_log_log_summary(records,xlabel,ylabel,B=20,summary=np.median,discrete=False):
     fig, ax = plt.subplots()
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    # Selecting only positive values
     records = records[np.array([idx for idx in range(records.shape[0]) if records[idx, 0]>0 and records[idx,1] > 0]),:]
-
-
     xs = [np.log(record[0]) for record in records]
     ys = [np.log(record[1]) for record in records]
     plt.scatter([(x) for x in xs], [(y) for y in  ys],s=0.5)
-
-
     if discrete==False:
         x_max = max(xs) - min(xs)
         # bin into default 20 values
-        binned_xs = [min(xs)+(x_max*i)/float(B) for i in range(B+1)]
+        binned_xs = [min(xs)+(x_max*i)/float(B) for i in range(B+2)]
         median_ys = []
         err = []
         for i in range(B+1):
@@ -48,8 +47,7 @@ def plot_log_log_summary(records,B=20,summary=np.median, discrete=False):
             median_ys += [current_y]
             #err += [[median_ys-np.std(current_ys)*0.5,median_ys+np.std(current_ys)*0.5]]
             err += [np.std(current_ys)]
-        plt.errorbar((binned_xs[-1:]+binned_xs[1:])/2, median_ys, marker='o', yerr=err,color='r')
-        print("Hi")
+        plt.errorbar((np.asarray(binned_xs[:-1])+np.asarray(binned_xs[1:]))/2.0, median_ys, marker='o', yerr=err,color='r')
     else:
         binned_xs = np.unique(xs)
         median_ys = []
@@ -116,6 +114,21 @@ def read_all_graphs(dir):
     G[e[0]][e[1]]['weight_inv_exp']=np.exp(-float(G[e[0]][e[1]]['weight'])/3.0)
     return G
 
+def bad_sim_score_1(G):
+    pwl = []
+    for e in tqdm(G.edges()):
+        wt = G[e[0]][e[1]]['weight']#/float(G.degree(e[0],weight='weight')+G.degree(e[1],weight='weight'))
+        nbr1 = set(G.neighbors(e[0]))
+        nbr2 = set(G.neighbors(e[1]))
+        common_nbrs= set.intersection(nbr1,nbr2)
+        all_nbrs = set.union(nbr1,nbr2)
+        #sim = sum([1/np.log(len(oregon_G.neighbors(v))) for v in common_nbrs])/sum(
+        #    [1/np.log(len(oregon_G.neighbors(v))) for v in all_nbrs])
+        sim = len(set.intersection(nbr1,nbr2))/float(len(set.union(nbr1,nbr2)))
+        #sim = len(set.intersection(nbr1,nbr2))
+        pwl += [(wt, sim)]
+    return np.asarray(pwl)
+
 def sim_score_1(G):
     pwl = []
     for e in tqdm(G.edges()):
@@ -147,9 +160,10 @@ def sim_score_2(G):
     return np.asarray(pwl)
 
 
-def self_similarity(G, distances):
+def self_similarity(G, distances,size=1000, weight='weight_inv'):
     pwl = []
-    for node in tqdm(np.random.choice(G.nodes(),size=1000)):
+    for node in tqdm(np.random.choice(G.nodes(),size=size)):
         for d in distances:
-            pwl += [(d,len(nx.single_source_dijkstra_path_length(G,node,cutoff=d,weight='weight_inv')))]
+            pwl += [(d,len(nx.single_source_dijkstra_path_length(G,node,cutoff=d,weight=weight)))]
     pwl=np.asarray(pwl)
+    return pwl
