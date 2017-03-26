2cf08b417bca35277b663094c4ea4f322c60fe15
Vaishnavh <vaishnavh.nagarajan@gmail.com>
2017-03-26 19:37:09 -0400

    Cleaned up code

--- utils.py
+++ utils.py
@@ -30,13 +30,13 @@ def plot(records):
     
 def plot_log_log_summary(records,xlabel,ylabel,B=20,summary=np.median,discrete=False):
     fig, ax = plt.subplots()
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
+
     # Selecting only positive values
     records = records[np.array([idx for idx in range(records.shape[0]) if records[idx, 0]>0 and records[idx,1] > 0]),:]
     xs = [np.log(record[0]) for record in records]
     ys = [np.log(record[1]) for record in records]
     plt.scatter([(x) for x in xs], [(y) for y in  ys],s=0.5)
+    m=0
     if discrete==False:
         x_max = max(xs) - min(xs)
         # bin into default 20 values
@@ -51,6 +51,7 @@ def plot_log_log_summary(records,xlabel,ylabel,B=20,summary=np.median,discrete=F
             #err += [[median_ys-np.std(current_ys)*0.5,median_ys+np.std(current_ys)*0.5]]
             err += [np.std(current_ys)]
         plt.errorbar((np.asarray(binned_xs[:-1])+np.asarray(binned_xs[1:]))/2.0, median_ys, marker='o', yerr=err,color='r')
+        m, c= np.polyfit((np.asarray(binned_xs[:-1])+np.asarray(binned_xs[1:]))/2.0, median_ys,1)
     else:
         binned_xs = np.unique(xs)
         median_ys = []
@@ -62,7 +63,9 @@ def plot_log_log_summary(records,xlabel,ylabel,B=20,summary=np.median,discrete=F
             #err += [[median_ys-np.std(current_ys)*0.5,median_ys+np.std(current_ys)*0.5]]
             err += [np.std(current_ys)]
         plt.errorbar(binned_xs, median_ys, marker='o', yerr=err,color='r')
-
+        m,c=(np.polyfit(binned_xs, median_ys, 1))
+    ax.set_xlabel(xlabel+':  Slope='+str(m))
+    ax.set_ylabel(ylabel)
 
 def plot_summary(records,xlabel,ylabel,B=20,summary=np.median,discrete=False):
     fig, ax = plt.subplots()
@@ -99,18 +102,42 @@ def plot_summary(records,xlabel,ylabel,B=20,summary=np.median,discrete=False):
         plt.errorbar(binned_xs, median_ys, marker='o', yerr=err,color='r')
 
 
-def read_all_graphs(dir):
-    G=nx.Graph()
-    for filename in tqdm(os.listdir(dir)):
-        temp_G = nx.read_adjlist(dir+filename,nodetype=int)
-        for e in (temp_G.edges_iter()):
-            if G.has_edge(e[0],e[1]):
-                G[e[0]][e[1]]['weight']+=1
-            else:
-                G.add_edge(e[0],e[1],weight=1)
-    G[e[0]][e[1]]['weight_inv']=1/float(G[e[0]][e[1]]['weight'])
-    G[e[0]][e[1]]['weight_inv_exp']=np.exp(-float(G[e[0]][e[1]]['weight'])/3.0)
-    return G
+def read_all_graphs(dir,directed=False):
+    if directed==False:
+        G=nx.Graph()    
+        for filename in tqdm(os.listdir(dir)):
+            temp_G = nx.read_adjlist(dir+filename,nodetype=int)
+
+            for e in (temp_G.edges_iter()):
+                if G.has_edge(e[0],e[1]):
+                    G[e[0]][e[1]]['weight']+=1
+                else:
+                    G.add_edge(e[0],e[1],weight=1)
+                G[e[0]][e[1]]['weight_inv']=1/float(G[e[0]][e[1]]['weight'])
+                G[e[0]][e[1]]['weight_inv_exp']=np.exp(-float(G[e[0]][e[1]]['weight'])/3.0)
+        return G
+    else:
+        diG=nx.DiGraph()
+        for filename in tqdm(os.listdir(dir)):
+            temp_G = np.loadtxt(dir+filename) #nx.read_adjlist(dir+filename,nodetype=int)
+            for i in range(temp_G.shape[0]):
+                e = temp_G[i]
+            #for e in (temp_G.edges_iter()):
+                if diG.has_edge(e[0],e[1]):
+                    diG[e[0]][e[1]]['weight']+=1
+                else:
+                    diG.add_edge(e[0],e[1],weight=1)
+        G=nx.Graph()
+        for e in diG.edges_iter():
+            for_wt = diG[e[0]][e[1]]['weight']
+            if e[0] < e[1]:
+                back_wt = 0
+                if diG.has_edge(e[1],e[0]):
+                    back_wt = diG[e[1]][e[0]]['weight']
+                    G.add_edge(e[0],e[1],weight=back_wt*for_wt)
+                    G[e[0]][e[1]]['weight_inv']=1/float(G[e[0]][e[1]]['weight'])
+                    G[e[0]][e[1]]['weight_inv_exp']=np.exp(-float(G[e[0]][e[1]]['weight'])/3.0)
+        return G
 
 
 def read_graph(filename):
@@ -166,7 +193,7 @@ def mutual(G):
 def mutual_ew(G):
     pwl = []
     for e in tqdm(G.edges()):
-        wt = G[e[0]][e[1]]['weight']#/float(G.degree(e[0],weight='weight')+G.degree(e[1],weight='weight'))
+        wt = G[e[0]][e[1]]['weight']
         nbr1 = set(G.neighbors(e[0]))
         nbr2 = set(G.neighbors(e[1]))
         common_nbrs= set.intersection(nbr1,nbr2)
@@ -242,7 +269,7 @@ def self_similarity(G, distances,size=1000, weight=None):
     if weight == None:
         for node in tqdm(np.random.choice(G.nodes(),size=size)):
             for d in distances:
-                pwl += [(d,len(nx.single_source_dijkstra_path_length(G,node,cutoff=d,weight=weight)))]        
+                pwl += [(d,len(nx.single_source_dijkstra_path_length(G,node,cutoff=d)))]        
     else:
         for node in tqdm(np.random.choice(G.nodes(),size=size)):
             for d in distances:
@@ -250,14 +277,7 @@ def self_similarity(G, distances,size=1000, weight=None):
     pwl=np.asarray(pwl)
     return pwl
 
-
-def self_similarity(G, distances,size=1000, weight='weight_inv'):
-    pwl = []
-    for node in tqdm(np.random.choice(G.nodes(),size=size)):
-        for d in distances:
-            pwl += [(d,len(nx.single_source_dijkstra_path_length(G,node,cutoff=d,weight=weight)))]
-    pwl=np.asarray(pwl)
-    return pwl    
+  
 
 def ewpl(G):
     pwl = []
